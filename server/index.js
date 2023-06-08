b7257c21a745f06896bddd4c2b09899856ab0a92
Priyanshu Pathak <125303909+Priyanshu1017@users.noreply.github.com>
2023-06-09 00:09:07 +0530

    Added production code

--- server/index.js
+++ server/index.js
@@ -9,12 +9,28 @@ env.config()
 connectDB();
 const app = express()
 
-const port = 5000
+const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
+
+/*****************PRODUCTION*******************/
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV==='production'){
+  app.use(express.static(path.join(__dirname1,'/client/build')))
+  app.get('*',(req,res)=>{
+      res.sendFile(path.resolve(__dirname1,'client','build','index.html'))
+    }
+  )
+}
+else{
+  app.get('/',(req,res)=>{
+    res.send('API is not running')
+  })
+}
+/*****************PRODUCTION*******************/
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
